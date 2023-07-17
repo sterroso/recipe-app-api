acaed6fe3e0c5e86923799e09ae0a73eeae8509a
Sergio Terroso <sergio.terroso@gmail.com>
2023-07-17 17:34:39 -0600

    fix: wait_for_db_command test

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -36,4 +36,4 @@ class CommandTests(SimpleTestCase):
         call_command('wait_for_db')
 
         self.assertEqual(patched_check.call_count, total_calls)
-        patched_check.assert_called_with(database=['default'])
+        patched_check.assert_called_with(databases=['default'])
