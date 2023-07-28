4a6eabde0191198fd55d5395619c94dc56defdda
Sergio Terroso <sergio.terroso@gmail.com>
2023-07-27 19:11:20 -0600

    add: AWS Docker deployment setup.

--- app/app/settings.py
+++ app/app/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+# flake8: noqa
 import os
 from pathlib import Path
 
@@ -21,12 +21,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y8vc)%qrdb558)zu&ofc(z@w77c*5cr-tuuqwc(!goid0!4$2p'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').
+        split(','),
+    )
+)
 
 
 # Application definition
@@ -113,9 +120,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'es-mx'
+LANGUAGE_CODE = os.environ.get('LANGUAGE', 'es-mx')
 
-TIME_ZONE = 'America/Mexico_City'
+TIME_ZONE = os.environ.get('PROJECT_TZ', 'America/Mexico_City')
 
 USE_I18N = True
 
